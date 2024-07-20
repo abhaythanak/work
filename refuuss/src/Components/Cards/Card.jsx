c1f8bd6dd9fbb1e038b7ae6ec4ee848379cbe759
abhaythanak <abhaythanak@gmail.com>
2024-07-20 12:20:22 +0530

    card section done hover remaining

--- refuuss/src/Components/Cards/Card.jsx
+++ refuuss/src/Components/Cards/Card.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({width, start, para}) {
   return (
-    <div className="w-1/2 bg-zinc-800 p-5 rounded-xl">
+    <div className={`${width}  bg-zinc-800 p-5 rounded-xl min-h-[30rem] flex flex-col justify-between`}>
       <div className="w-full">
         <div className="w-full flex justify-between items-center">
           <h3>one heading</h3>
           <span>Arrow</span>
         </div>
         <h1 className="text-3xl font-medium mt-10">whatever heading</h1>
+      </div>
+      <div className="down w-full ">
+        {
+          start &&  (
+            <>
+            <h1 className="text-6xl font-semibold tracking-tight leading-none">start a project</h1>
+            <button className="rounded-full mt-5 py-2 px-5 border border-zinc-100">Contact Us</button>
+            </>
+          
+          )
+        }
+        {
+          para &&  (
+            <>
+            <p className=" bottom-3 text-sm mt-5 text-zinc-500 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates?</p>
+            </>
+          
+          )
+        }
+        
       </div>
     </div>
   );
--- refuuss/src/Components/Cards/Cards.jsx
+++ refuuss/src/Components/Cards/Cards.jsx
@@ -5,8 +5,8 @@ export default function Cards() {
   return (
     <div className='w-full'>
       <div className="flex gap-5 bg-zinc-500 max-w-screen-xl mx-auto py-32">
-        <Card/>
-        <Card/>
+        <Card width={"basis-1/3"} start={false} para={true}/>
+        <Card width={"basis-2/3"} start={true} para={false}/>
       </div>
     </div>
   )
